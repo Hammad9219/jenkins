3f46acf6af3bd16177537cc417e34506ea858528
Mohammad Hammad <mohammad.hammad@nagarro.com>
2022-12-28 12:49:08 +0530

    hm

--- obj/Release/netcoreapp3.1/TestApp.AssemblyInfo.cs
+++ obj/Release/netcoreapp3.1/TestApp.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("71d56d85-7a95-4147-8143-496a9bc42351")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
